69acf2b5054f8c805f35d5b8a674a9ff74d9b512
Taylor Elder <54559165+aelder202@users.noreply.github.com>
2024-07-31 08:01:37 -0400

    Revert "Modal update"
    This reverts commit 14f3409cfd3738e1c77403d2697cd2b391c0312b.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,171 +4,169 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
+(function($) {
 
-	var $window = $(window),
+	var	$window = $(window),
 		$body = $('body'),
 		$nav = $('#nav');
-
+	
 	// Modal Implementation
-	document.addEventListener('DOMContentLoaded', function() {
-		var modals = document.querySelectorAll('.modal_multi');
-		var btns = document.querySelectorAll('.myBtn_multi');
-		var closeButtons = document.querySelectorAll('.close_multi');
-	
-		// Function to close all modals
-		function closeAllModals() {
-			modals.forEach(modal => {
-				modal.style.display = 'none';
-			});
+	// Get the modal
+	var modalparent = document.getElementsByClassName("modal_multi");
+
+	// Get the button that opens the modal
+
+	var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+	// Get the <span> element that closes the modal
+	var span_close_multi = document.getElementsByClassName("close_multi");
+
+	function setDataIndex() {
+
+		for (i = 0; i < modal_btn_multi.length; i++)
+		{
+			modal_btn_multi[i].setAttribute('data-index', i);
+			modalparent[i].setAttribute('data-index', i);
+			span_close_multi[i].setAttribute('data-index', i);
 		}
-	
-		// Open modal when a button is clicked
-		btns.forEach(button => {
-			button.onclick = function() {
-				// Close all other modals
-				closeAllModals();
-	
-				// Open the corresponding modal
-				var modal = button.nextElementSibling;
-				if (modal && modal.classList.contains('modal_multi')) {
-					modal.style.display = 'block';
-				}
-			};
-		});
-	
-		// Close modal when the close button is clicked
-		closeButtons.forEach(closeButton => {
-			closeButton.onclick = function() {
-				var modal = closeButton.closest('.modal_multi');
-				if (modal) {
-					modal.style.display = 'none';
-				}
-			};
-		});
-	
-		// Close the top-most modal when clicking outside of it
-		window.addEventListener('click', function(event) {
-			var openModals = Array.from(modals).filter(modal => modal.style.display === 'block');
-			if (openModals.length > 0) {
-				var topModal = openModals[openModals.length - 1];
-				if (!topModal.querySelector('.modal-content').contains(event.target)) {
-					topModal.style.display = 'none';
-				}
-			}
-		});
-	});
-	
-	  
+	}
+
+
+
+	for (i = 0; i < modal_btn_multi.length; i++)
+	{
+		modal_btn_multi[i].onclick = function() {
+			var ElementIndex = this.getAttribute('data-index');
+			modalparent[ElementIndex].style.display = "block";
+		};
+
+		// When the user clicks on <span> (x), close the modal
+		span_close_multi[i].onclick = function() {
+			var ElementIndex = this.getAttribute('data-index');
+			modalparent[ElementIndex].style.display = "none";
+		};
+
+	}
+
+	window.onload = function() {
+
+		setDataIndex();
+	};
+
+	window.onclick = function(event) {
+		if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+			modalparent[event.target.getAttribute('data-index')].style.display = "none";
+		}
+	};
 
 	// Breakpoints.
-	breakpoints({
-		wide: ['961px', '1880px'],
-		normal: ['961px', '1620px'],
-		narrow: ['961px', '1320px'],
-		narrower: ['737px', '960px'],
-		mobile: [null, '736px']
-	});
-
-	// Play initial animations on page load.
-	$window.on('load', function () {
-		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
-
-	// Nav.
-	var $nav_a = $nav.find('a');
-
-	$nav_a
-		.addClass('scrolly')
-		.on('click', function (e) {
-
-			var $this = $(this);
-
-			// External link? Bail.
-			if ($this.attr('href').charAt(0) != '#')
-				return;
-
-			// Prevent default.
-			e.preventDefault();
-
-			// Deactivate all links.
-			$nav_a.removeClass('active');
-
-			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-			$this
-				.addClass('active')
-				.addClass('active-locked');
-
-		})
-		.each(function () {
-
-			var $this = $(this),
-				id = $this.attr('href'),
-				$section = $(id);
-
-			// No section for this link? Bail.
-			if ($section.length < 1)
-				return;
-
-			// Scrollex.
-			$section.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function () {
-
-					// Deactivate section.
-					$section.addClass('inactive');
-
-				},
-				enter: function () {
-
-					// Activate section.
-					$section.removeClass('inactive');
-
-					// No locked links? Deactivate all links and activate this section's one.
-					if ($nav_a.filter('.active-locked').length == 0) {
-
-						$nav_a.removeClass('active');
-						$this.addClass('active');
-
-					}
-
-					// Otherwise, if this section's link is the one that's locked, unlock it.
-					else if ($this.hasClass('active-locked'))
-						$this.removeClass('active-locked');
-
-				}
-			});
-
+		breakpoints({
+			wide:      [ '961px',  '1880px' ],
+			normal:    [ '961px',  '1620px' ],
+			narrow:    [ '961px',  '1320px' ],
+			narrower:  [ '737px',  '960px'  ],
+			mobile:    [ null,     '736px'  ]
 		});
 
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Nav.
+		var $nav_a = $nav.find('a');
+
+		$nav_a
+			.addClass('scrolly')
+			.on('click', function(e) {
+
+				var $this = $(this);
+
+				// External link? Bail.
+					if ($this.attr('href').charAt(0) != '#')
+						return;
+
+				// Prevent default.
+					e.preventDefault();
+
+				// Deactivate all links.
+					$nav_a.removeClass('active');
+
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+					$this
+						.addClass('active')
+						.addClass('active-locked');
+
+			})
+			.each(function() {
+
+				var	$this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
+
+				// No section for this link? Bail.
+					if ($section.length < 1)
+						return;
+
+				// Scrollex.
+					$section.scrollex({
+						mode: 'middle',
+						top: '-10vh',
+						bottom: '-10vh',
+						initialize: function() {
+
+							// Deactivate section.
+								$section.addClass('inactive');
+
+						},
+						enter: function() {
+
+							// Activate section.
+								$section.removeClass('inactive');
+
+							// No locked links? Deactivate all links and activate this section's one.
+								if ($nav_a.filter('.active-locked').length == 0) {
+
+									$nav_a.removeClass('active');
+									$this.addClass('active');
+
+								}
+
+							// Otherwise, if this section's link is the one that's locked, unlock it.
+								else if ($this.hasClass('active-locked'))
+									$this.removeClass('active-locked');
+
+						}
+					});
+
+			});
+
 	// Scrolly.
-	$('.scrolly').scrolly();
+		$('.scrolly').scrolly();
 
 	// Header (narrower + mobile).
 
-	// Toggle.
-	$(
-		'<div id="headerToggle">' +
-		'<a href="#header" class="toggle"></a>' +
-		'</div>'
-	)
-		.appendTo($body);
+		// Toggle.
+			$(
+				'<div id="headerToggle">' +
+					'<a href="#header" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
 
-	// Header.
-	$('#header')
-		.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'left',
-			target: $body,
-			visibleClass: 'header-visible'
-		});
+		// Header.
+			$('#header')
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'header-visible'
+				});
 
 })(jQuery);
